2595efda94320e9a9e0664661828849f0127be15
Jens Reinecke <jens.reinecke@arm.com>
2025-11-27 13:25:08 +0100

    Use latest pyOCD release build for nightlies (#659)
    * Bump @open-cmsis-pack/vsce-helper to v0.2.0
    * Download tools script to be configurable for nightly builds
    * Read permissions for actions and GH token for octokit

--- scripts/download-tools.ts
+++ scripts/download-tools.ts
@@ -16,27 +16,29 @@
  * limitations under the License.
  */
 
-import { ArchiveFileAsset, Downloadable, Downloader, GitHubReleaseAsset, WebFileAsset  } from '@open-cmsis-pack/vsce-helper';
+import { ArchiveFileAsset, Downloadable, Downloader, GitHubReleaseAsset, GitHubWorkflowAsset, WebFileAsset  } from '@open-cmsis-pack/vsce-helper';
 import { PackageJson } from 'type-fest';
 import fs from 'node:fs/promises';
 import path from 'node:path';
+import process from 'node:process';
 
 type CmsisPackageJson = PackageJson & {
     cmsis: {
         pyocd?: string;
+        pyocdNightly?: string;
         gdb?: string;
     };                                                                                                                                                                                                                                                                                
 };
 
-function splitGitReleaseVersion(version: string, owner: string, repo: string) {
-    if (version.includes('@')) {
-        const parts = version.split('@');
-        version = parts[1];
+function splitGitReference(reference: string, owner: string, repo: string) {
+    if (reference.includes('@')) {
+        const parts = reference.split('@');
+        reference = parts[1];
         const repoAndOwner = parts[0].split('/');
         repo = repoAndOwner[1]
         owner = repoAndOwner[0];
     }
-    return { repo, owner, version };
+    return { repo, owner, reference };
 }
 
 const pyocd : Downloadable = new Downloadable(
@@ -56,12 +58,41 @@ const pyocd : Downloadable = new Downloadable(
             console.warn('No pyOCD version specified in package.json');
             return undefined;
         }
-        const { repo, owner, version } = splitGitReleaseVersion(reqVersion, 'pyocd', 'pyOCD');
+        // Here, reference is expected to be a release asset version
+        const { repo, owner, reference } = splitGitReference(reqVersion, 'pyocd', 'pyOCD');
         const releaseAsset = new GitHubReleaseAsset(
-            owner, repo, version,
-            `pyocd-${os}${arch}-${version}.zip`, 
+            owner, repo, reference,
+            `pyocd-${os}${arch}-${reference}.zip`, 
             { token: process.env.GITHUB_TOKEN });
         const asset = new ArchiveFileAsset(releaseAsset);
+        return asset;
+    },
+)
+
+const pyocdNightly : Downloadable = new Downloadable(
+    'pyOCD', 'pyocd',
+    async (target) => {
+        const { os, arch } = {
+            'win32-x64': { os: 'windows', arch: '' },
+            'win32-arm64': { os: 'windows', arch: '' },
+            'linux-x64': { os: 'linux', arch: '' },
+            'linux-arm64': { os: 'linux', arch: '-arm64' },
+            'darwin-x64': { os: 'macos', arch: '' },
+            'darwin-arm64': { os: 'macos', arch: '' },
+        }[target];
+        const json = await downloader.getPackageJson<CmsisPackageJson>();
+        const workflow = json?.cmsis?.pyocdNightly;
+        if (workflow === undefined) {
+            console.warn('No pyOCD \'Nightly\' workflow specified in package.json (<repo>@<workflowname>)');
+            return undefined;
+        }
+        // Here, reference is expected to be the name of the workflow yaml file without file ending
+        const { repo, owner, reference } = splitGitReference(workflow, 'pyocd', 'pyOCD');
+        const assetPattern = (`pyocd-${os}${arch}-*`);
+        const asset = new GitHubWorkflowAsset(
+            owner, repo, `${reference}.yaml`,
+            assetPattern, 
+            { token: process.env.GITHUB_TOKEN });
         return asset;
     },
 )
@@ -98,7 +129,13 @@ const gdb : Downloadable = new Downloadable(
     },
 );
 
-const downloader = new Downloader({ pyocd, gdb });
+// If no arguments are provided to the downloader script, all assets are downloaded
+// in the order they are listed. In that case, 'pyocd' will overwrite 'pyocdNightly'.
+const downloader = new Downloader({
+    pyocdNightly,
+    pyocd,
+    gdb
+});
 
 downloader
     .withCacheDir(await downloader.defaultCacheDir())
